2f48dc7cb68d55d2c3fa6475f69c795b41ccf178
RedsMod721 <steevi38000@gmail.com>
2026-02-04 16:06:34 +0100

    Create project structure (Week 1)

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-# config.py
-
 # MySQL connection URL for the journaling app
-DATABASE_URL = "mysql+pymysql://Seb:Scon13%404D@localhost:3306/journal_app_db"
+#DATABASE_URL = "mysql+pymysql://Seb:Scon13%404D@localhost:3306/journal_app_db"
 
+# PostgreSQL connection URL for the journaling app
+DATABASE_URL = "postgresql+psycopg2://postgres:Scon13%404D@localhost:5432/journal_app_db"
 
 # Additional global configurations can go here in the future
